da02edf44053361c8b21ae5e8bca465a15fd0ca7
fabienGouttengre <112575249+fabienGouttengre@users.noreply.github.com>
2023-03-21 03:20:24 +0100

    feat / main

---
+++ midiWhammy/midiWhammy.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		midiWhammy.ino
+ Created:	21/03/2023 03:10:34
+ Author:	fabien goutttenegre
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
